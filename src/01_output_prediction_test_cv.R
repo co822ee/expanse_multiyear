def08b3c9ba8e5fc08cf1b727de342a255510f24
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-06-15 20:01:29 +0200

    change parameter

--- src/01_output_prediction_test_cv.R
+++ src/01_output_prediction_test_cv.R
@@ -1,12 +1,11 @@
 source("../EXPANSE_algorithm/scr/fun_call_lib.R")
 source("src/00_fun_read_data.R")
 # Multiple single years
-target_poll <- 'NO2'
-
+target_poll <- 'PM2.5'
 csv_names <- paste0('o2_',target_poll, "_",c('08-10', '09-11', '10-12', 
-                                             '08-12', '06-12', '12-18', '00-19'))   #2008:2012
+                                              '08-12', '06-12', '12-18', '00-18'))   #2008:2012
 years <- list(2008:2010, 2009:2011, 2010:2012, 
-              2008:2012, 2006:2012, 2012:2018, 2000:2019)
+              2008:2012, 2006:2012, 2012:2018, 2000:2018)
 # list.files('data/workingData/', paste0('SLR_result_all_o2_', target_poll))
 
 nfold=5
@@ -34,7 +33,7 @@ write_output_5csv <- function(year_i){
    rf_test <- do.call(rbind, rf_test)
 
    all_test <- cbind(rf=rf_test$rf, slr_test)  #gwr=gwr_test$gwr  #slr_rf=slr_rf_test$slr_rf,
-   write.csv(all_test, paste0("data/workingData/5cv_", csv_names[year_i], ".csv"))
+   write.csv(all_test, paste0("data/workingData/5cv_", csv_names[year_i], ".csv"), row.names = F)
 
 }
 lapply(seq_along(csv_names), write_output_5csv)
