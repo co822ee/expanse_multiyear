61995373b8dbbe06ca2c5c310b66b10db0984dd0
Youchen Shen (mac uu) <co822ee@gmail.com>
2021-07-22 13:06:17 +0200

    put function up front

--- src/00_gtwr.R
+++ src/00_gtwr.R
@@ -1,8 +1,8 @@
 #------ 1) Read in data ------
 source("../EXPANSE_algorithm/scr/fun_call_lib.R")
-# Whether to tune RF
-tuneRF_b = F
-target_poll = 'PM2.5'
+library(doParallel)
+library(foreach)
+target_poll = 'NO2'
 obs_varname = 'obs'
 # Multiple single years
 csv_names <- paste0('o3_',target_poll, "_",c('08-10', '09-11', '10-12', 
@@ -10,17 +10,9 @@ csv_names <- paste0('o3_',target_poll, "_",c('08-10', '09-11', '10-12',
 years <- list(2008:2010, 2009:2011, 2010:2012, 
               2008:2012, 2006:2012, 2012:2019, 2000:2019)
 nfold <- 5
-yr_i <- 6
+yr_i <- 1
 # gtwr_yr <- 2010
-csv_name <- csv_names[yr_i]
-print("********************************************")
-print(csv_name)
-
-source("../expanse_multiyear/src/00_fun_read_data_gee.R")
-
-csv_name <- csv_names[yr_i]
-df_sub <- read_data(target_poll, years[[yr_i]])
-fold_i <- 1
+# fold_i <- 1
 ## ------ *** useful function -----
 creat_spPoints <- function(data_df, xcoord, ycoord, crs_str){
    sp::SpatialPointsDataFrame(data = data_df, 
@@ -60,8 +52,30 @@ gen_df_gtwr <- function(gtwr_model, sp_p, df_p, forstepwise=F){
    }
    gwr_test_df
 }
+
+run_GTWR <- function(fold2_i, grid_i, yrs_v){
+   #------ 4) Subset training data into training and validation data ------
+   print('------------------------------')
+   print(paste0('fold ', fold2_i))
+   train_sub2 <- train_sub %>% dplyr::select(-c('nfold', 'n_obs', 'index'))
+   data_all2 <- create_fold(train_sub2, seed, strt_group=c("n_obs", "sta_type", "zoneID"), 
+                            multiyear_group = c("sta_code", "year"),
+                            nfold = 5)
+   valid_sub <- data_all2[data_all2$nfold==fold2_i,]
+   train_sub2 <- data_all2[-valid_sub$index, ] #data_all$index starts from 1 to the length.
+   
+   sp_train2 <- creat_spPoints(train_sub2, 'xcoord', 'ycoord', local_crs)
+   sp_valid <- creat_spPoints(valid_sub, 'xcoord', 'ycoord', local_crs)
+   
+   # Run the GTWR for all years in the period
+   gtwr_valid <- lapply(yrs_v, trainGTWR, sp_train1=sp_train2, grid_i=grid_i,
+                        sp_valid1=sp_valid, valid_sub1=valid_sub, 
+                        target_poll=target_poll, eq=eq) %>% do.call(rbind, .)  ## Should be the same size as validation data
+   gtwr_valid
+}
+
 ## Train GTWR for each year
-trainGTWR <- function(sp_train1, gtwr_yr, grid_i, sp_valid1, valid_sub1, target_poll){
+trainGTWR <- function(sp_train1, gtwr_yr, grid_i, sp_valid1, valid_sub1, target_poll, eq){
    print(paste0('gtwr_yr ', gtwr_yr))
    if(target_poll=='PM2.5'|target_poll=='PM10'){
       sp_train_sub <- sp_train1[sp_train1$year%in%seq(gtwr_yr-1, gtwr_yr+1, 1),]
@@ -98,9 +112,13 @@ trainGTWR <- function(sp_train1, gtwr_yr, grid_i, sp_valid1, valid_sub1, target_
                          reg.tv=rep(gtwr_yr, nrow(coordinates(grd2))),     ## time stamps for the regression points
                          adaptive=T, kernel='exponential'), 
                          error=function(e) T)
-      if(!any(is.na(gtwr_model$SDF[1]$Intercept))&typeof(gtwr_model)!='logical'){
-         gtwr_valid <- gen_df_gtwr(gtwr_model, sp_valid_sub, valid_sub1)
-         return(gtwr_valid)
+      if(typeof(gtwr_model)!='logical'){
+         if(!any(is.na(gtwr_model$SDF[1]$Intercept))){
+            gtwr_valid <- gen_df_gtwr(gtwr_model, sp_valid_sub, valid_sub1)
+            return(gtwr_valid)
+         }else{
+            return(cbind(data.frame(gtwr=NA), valid_sub1))
+         }
       }else{
          return(cbind(data.frame(gtwr=NA), valid_sub1))
       }
@@ -110,66 +128,65 @@ trainGTWR <- function(sp_train1, gtwr_yr, grid_i, sp_valid1, valid_sub1, target_
       return(cbind(data.frame(gtwr=NA), valid_sub1))
    }
 }
-#------ 2) Subset training and test for 5-CV (this should be the same as slr and rf) ------
-source("src/00_fun_create_fold.R")
-# The stations will only be included in one specific fold.
-data_all <- create_fold(df_sub, seed, strt_group=c("n_obs", "sta_type", "zoneID"), 
-                        multiyear_group = c("sta_code", "year"),
-                        nfold = 5)
-test_sub <- data_all[data_all$nfold==fold_i,]
-train_sub <- data_all[-test_sub$index, ] #data_all$index starts from 1 to the length.
 
-sp_train <- creat_spPoints(train_sub, 'xcoord', 'ycoord', local_crs)
-sp_test <- creat_spPoints(test_sub, 'xcoord', 'ycoord', local_crs)
-
-#------ 3) Read in the predictors selected by SLR (using the same full training data) ------
-
-
-slr <- read.csv(paste0("data/workingData/SLR_summary_model_", csv_name, '_fold_', fold_i, '.csv'))
-eq <- as.formula(paste0(obs_varname, '~',  paste(slr$variables[-1], collapse = "+")))
-
-grd2 <- create_regressionGrid(eu_bnd, 200000, local_crs)
-# Create a grid of parameters (lamda & ksi)  using expand.grid
-gtwr_param <- expand.grid(lamda=seq(0, 1, 0.1), ksi=seq(0, pi/2, pi/8))
-
-#------ 4) Subset training data into training and validation data ------
-# paramGrid_i <- 4  # Loop
-# fold2_i <- 1  # Loop
-run_GTWR <- function(fold2_i, grid_i, yrs_v){
-   print('------------------------------')
-   print(paste0('fold ', fold2_i))
-   train_sub2 <- train_sub %>% dplyr::select(-c('nfold', 'n_obs', 'index'))
-   data_all2 <- create_fold(train_sub2, seed, strt_group=c("n_obs", "sta_type", "zoneID"), 
-                            multiyear_group = c("sta_code", "year"),
-                            nfold = 5)
-   valid_sub <- data_all2[data_all2$nfold==fold2_i,]
-   train_sub2 <- data_all2[-valid_sub$index, ] #data_all$index starts from 1 to the length.
-   
-   sp_train2 <- creat_spPoints(train_sub2, 'xcoord', 'ycoord', local_crs)
-   sp_valid <- creat_spPoints(valid_sub, 'xcoord', 'ycoord', local_crs)
-   
-   # Run the GTWR for all years in the period
-   gtwr_valid <- lapply(yrs_v, trainGTWR, sp_train1=sp_train2, grid_i=grid_i,
-                        sp_valid1=sp_valid, valid_sub1=valid_sub, target_poll=target_poll) %>% do.call(rbind, .)  ## Should be the same size as validation data
-   gtwr_valid
-}
-
-
-# end the fold2_i loop
-#------ 8) Evaluate the performance for GTWR model ------
-# Remove NA values for gtwr
 calibr_GTWR <- function(paramGrid_i, nfold, yrs_v){
    print(paste0('paramGrid_i: ', paramGrid_i))
    gtwr_valid <- lapply(1:nfold, run_GTWR, grid_i=paramGrid_i, yrs_v) %>% do.call(rbind, .) ## should be the same size as data_all2
    gtwr_valid2 <- gtwr_valid[!is.na(gtwr_valid$gtwr), ]
+   #------ 8) Evaluate the performance for GTWR model ------
+   # Remove NA values for gtwr
    cbind(t(error_matrix(gtwr_valid2$obs, gtwr_valid2$gtwr)), 
          gtwr_param[paramGrid_i, ],
-         data.frame(ndata=round(nrow(gtwr_valid2)/nrow(gtwr_valid)*100, 1)))
+         data.frame(ndata=round(nrow(gtwr_valid2)/nrow(gtwr_valid)*100, 1)))  
+   ## Need ndata to inspect whether there is enough amount of fold used for CV!
 }
-final_perfm <- lapply(1:(nrow(gtwr_param)), calibr_GTWR, nfold=nfold, yrs_v=years[[yr_i]])
-final_perfm2 <- final_perfm %>% do.call(rbind, .)
+for(yr_i in seq_along(csv_names)){
+   csv_name <- csv_names[yr_i]
+   print("********************************************")
+   print(csv_name)
+   
+   source("../expanse_multiyear/src/00_fun_read_data_gee.R")
+   
+   csv_name <- csv_names[yr_i]
+   df_sub <- read_data(target_poll, years[[yr_i]])
+   #------ 2) Subset training and test for 5-CV (this should be the same as slr and rf) ------
+   source("src/00_fun_create_fold.R")
+   # The stations will only be included in one specific fold.
+   data_all <- create_fold(df_sub, seed, strt_group=c("n_obs", "sta_type", "zoneID"), 
+                           multiyear_group = c("sta_code", "year"),
+                           nfold = 5)
+   cluster_no <- 5
+   cl <- parallel::makeCluster(cluster_no)
+   doParallel::registerDoParallel(cl)
+   foreach(fold_i = 1:nfold)  %dopar%  {
+      source("../EXPANSE_algorithm/scr/fun_call_lib.R")
+      test_sub <- data_all[data_all$nfold==fold_i,]
+      train_sub <- data_all[-test_sub$index, ] #data_all$index starts from 1 to the length.
+      
+      sp_train <- creat_spPoints(train_sub, 'xcoord', 'ycoord', local_crs)
+      sp_test <- creat_spPoints(test_sub, 'xcoord', 'ycoord', local_crs)
+      
+      #------ 3) Read in the predictors selected by SLR (using the same full training data) ------
+      
+      
+      slr <- read.csv(paste0("data/workingData/SLR_summary_model_", csv_name, '_fold_', fold_i, '.csv'))
+      eq <- as.formula(paste0(obs_varname, '~',  paste(slr$variables[-1], collapse = "+")))
+      
+      grd2 <- create_regressionGrid(eu_bnd, 200000, local_crs)
+      # Create a grid of parameters (lamda & ksi)  using expand.grid
+      gtwr_param <- expand.grid(lamda=seq(0, 1, 0.1), ksi=seq(0, pi/2, pi/8))
+      
+      final_perfm <- lapply(1:(nrow(gtwr_param)), calibr_GTWR, nfold=nfold, yrs_v=years[[yr_i]])
+      final_perfm2 <- final_perfm %>% do.call(rbind, .)
+      
+      write.csv(final_perfm2, paste0('data/workingData//gtwr_param_', csv_name, '_fold_', fold_i, '.csv'), row.names = F)
+      
+      #------ 9) Find the parameter set which gives the best RMSE/R2 ------
+      # lamda_final <- (final_perfm2 %>% arrange(desc(ndata), desc(rsq)))[1,]$lamda
+      # ksi_final <- (final_perfm2 %>% arrange(desc(ndata), desc(rsq)))[1,]$ksi
+   }
+   stopCluster(cl)
+}
 
-write.csv(final_perfm2, paste0('data/processed/gtwr_param_', csv_name, '.csv'), row.names = F)
-#------ 9) Find the parameter set which gives the best RMSE/R2 ------
 
 # Save this parameter optimization results
